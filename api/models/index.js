2f1f41d57e1bcb088814e565fe5e21ab090bed28
Fran <fraivan_1@hotmail.com>
2021-11-24 15:47:16 -0600

    Correcciones menores

--- api/models/index.js
+++ api/models/index.js
@@ -1,7 +1,7 @@
 const Equipo = require("./Equipo");
 const Usuario = require("./Usuario");
-// const Actividad = require("./Actividades");
+// const Actividad = require("./Actividad");
 
-Equipo.hasMany(Actividad);
+// Equipo.hasMany(Actividad);
 
 module.exports = { Usuario, Equipo };
