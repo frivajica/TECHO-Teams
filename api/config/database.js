f768df41d20e80086e308b1561cf50c710309b31
joaquin <joaquin.ecker@gmail.com>
2021-11-24 14:57:53 -0300

    Retoques conexion mysql

--- api/config/database.js
+++ api/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("dbtecho", "root", "techoGreatPassword", {
+const db = new Sequelize("dbtecho", "root", "password", {
   dialect: "mysql",
   dialectOptions: {
     // Your mysql2 options here
