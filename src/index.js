e2cd2aaa0b66430695b7677f03cc07c325f9faa1
Martin Paladino <c.martin.pal@gmail.com>
2021-11-25 12:25:09 -0300

    agrego el provider de redux en index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./state/store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
