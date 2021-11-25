894a706496bf5d2aa016b7db89029c7d09c4b17e
eber-cba <ebercoronel29@gmail.com>
2021-11-25 18:32:38 -0300

    Modal-Ingresar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import portada from "../../assets/imagenes/home/portada.jpg";
 import Cards from "../cards/Cards"
+import Box from "@mui/material/Box";
 
 const Home = () => {
 
   
   return (
-    <div>
+    <Box sx={{position:'fixed'}}>
       <img src={portada} alt="portada" width="100%" height="auto" />
       <Cards />
-    </div>
+    </Box>
   );
 };
 
