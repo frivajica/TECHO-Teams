b8f2535c37f5fc1e2729c186742b9e8a1b376a5e
Fran <fraivan_1@hotmail.com>
2021-11-25 11:31:58 -0600

    Menú de usuario añadido y margin del body arreglado a 0

--- src/commons/navbar/Navbar.jsx
+++ src/commons/navbar/Navbar.jsx
@@ -5,26 +5,39 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import logo from "../../assets/imagenes/navbar/logo.png";
-import theme from "../../views/themeConfig"
-import { ThemeProvider } from '@mui/material/styles';
+import theme from "../../views/themeConfig";
+import { ThemeProvider } from "@mui/material/styles";
+import { UserMenu } from "../../components/userMenu/UserMenu";
+import { usuario } from "../../utils/mockData";
+const techoRedirect = () => (window.location.href = "https://www.techo.org/");
 
 const Navbar = () => {
   return (
     <ThemeProvider theme={theme}>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={{ background: "#0092dd" }} >
-        <Toolbar >
-          <Box sx={{flexGrow:1, ml:5}}> 
-          <Link to="/">
-            <img src={logo} width="122px" height="37.6px" alt="logo"  />
-          </Link>
-          </Box>
-            <Button color="inherit" sx={{mr:5}}>
-              INGRESAR
-            </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar sx={{ background: "#0092dd" }}>
+          <Toolbar>
+            <Box sx={{ flexGrow: 1, ml: 5 }}>
+              <Link to="/">
+                <img
+                  src={logo}
+                  width="122px"
+                  height="37.6px"
+                  alt="logo"
+                  onClick={techoRedirect}
+                />
+              </Link>
+            </Box>
+            {usuario ? (
+              <UserMenu />
+            ) : (
+              <Button color="inherit" sx={{ mr: 5 }}>
+                INGRESAR
+              </Button>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
     </ThemeProvider>
   );
 };
---
+++ src/components/userMenu/UserMenu.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { usuario } from "../../utils/mockData";
+import "./UserMenu.css";
+
+export const UserMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="positioned-button"
+        aria-controls="demo-positioned-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        {usuario.nombre}
+      </Button>
+      <Menu
+        id="positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Mi Perfil</MenuItem>
+        {(usuario.cargo === "Coordinador" || usuario.cargo === "Admin") && (
+          <MenuItem onClick={handleClose}>Mis Equipos</MenuItem>
+        )}
+        {usuario.cargo === "Admin" && (
+          <MenuItem onClick={handleClose}>AdminLand</MenuItem>
+        )}
+        <MenuItem onClick={handleClose}>Cerrar Sesión</MenuItem>
+      </Menu>
+    </div>
+  );
+};
