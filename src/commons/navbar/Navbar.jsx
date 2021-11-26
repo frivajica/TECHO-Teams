4465268443293bb34d5e36ab13a9818c6f37ea5c
joaquin <joaquin.ecker@gmail.com>
2021-11-26 13:04:46 -0300

    Pre demo 1

--- src/commons/navbar/Navbar.jsx
+++ src/commons/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import logo from "../../assets/imagenes/navbar/logo.png";
 import LoginModal from "./LoginModal";
-import { usuario } from "../../utils/mockData"
-import { UserMenu } from "../../components/userMenu/UserMenu"
+import { usuario } from "../../utils/mockData";
+import { UserMenu } from "../../components/userMenu/UserMenu";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -16,21 +16,18 @@ const Navbar = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar id="navbar">
+      <AppBar id="navbar" position="sticky">
         <Toolbar>
           <Box sx={{ flexGrow: 1, ml: 5 }}>
             <Link to="/">
               <img id="logo" src={logo} alt="logo" />
             </Link>
           </Box>
-          {usuario ? (
-              <UserMenu />
-            ) : (
-              <Button onClick={handleOpen} color="inherit" sx={{ mr: 5 }}>
-                INGRESAR
-                <LoginModal open={open} handleClose={handleClose} />
-              </Button>
-            )}
+          {usuario && <UserMenu />}
+          <Button onClick={handleOpen} color="inherit" sx={{ mr: 5 }}>
+            INGRESAR
+          </Button>
+          <LoginModal open={open} handleClose={handleClose} />
         </Toolbar>
       </AppBar>
     </Box>
