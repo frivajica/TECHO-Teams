c63f68d9afdb42264abed044758576b1145d2691
Martin Paladino <c.martin.pal@gmail.com>
2021-11-26 11:08:02 -0300

    separacion de componentes

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,93 +1,7 @@
 import Link from "@mui/material/Link";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import ButtonBase from "@mui/material/ButtonBase";
 import Typography from "@mui/material/Typography";
-import actividades from "../../assets/imagenes/cards/actividades.jpg";
-import capacitaciones from "../../assets/imagenes/cards/capacitaciones.JPG";
-import intranet from "../../assets/imagenes/cards/intratecho.jpg";
-
-const plataformas = [
-  {
-    nombre: "ACTIVIDADES",
-    imagen: actividades,
-    url: "https://actividades.techo.org/",
-  },
-  {
-    nombre: "INTRANET",
-    imagen: intranet,
-    url: "https://sites.google.com/a/techo.org/intranet-argentina/home?authuser=0",
-  },
-  {
-    nombre: "CAPACITACIONES",
-    imagen: capacitaciones,
-    url: "https://sites.google.com/a/techo.org/intranet-argentina/areas/construcciones/capacitaciones",
-  },
-];
-
-const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: "relative",
-  height: 200,
-  [theme.breakpoints.down("sm")]: {
-    width: "100% !important", // Overrides inline-style
-    height: 100,
-  },
-  "&:hover, &.Mui-focusVisible": {
-    zIndex: 1,
-    "& .MuiImageBackdrop-root": {
-      opacity: 0.15,
-    },
-    "& .MuiImageMarked-root": {
-      opacity: 0,
-    },
-    "& .MuiTypography-root": {
-      border: "4px solid currentColor",
-    },
-  },
-}));
-
-const ImageSrc = styled("span")({
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundSize: "cover",
-  backgroundPosition: "center 40%",
-});
-
-const Image = styled("span")(({ theme }) => ({
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: theme.palette.common.white,
-}));
-
-const ImageBackdrop = styled("span")(({ theme }) => ({
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: 0.4,
-  transition: theme.transitions.create("opacity"),
-}));
-
-const ImageMarked = styled("span")(({ theme }) => ({
-  height: 3,
-  width: 18,
-  backgroundColor: theme.palette.common.white,
-  position: "absolute",
-  bottom: -2,
-  left: "calc(50% - 9px)",
-  transition: theme.transitions.create("opacity"),
-}));
+import { ImageButton, ImageSrc, Image, ImageBackdrop, ImageMarked, plataformas } from "../../utils/cardsUtils"
 
 const Cards = () => {
   return (
@@ -97,7 +11,7 @@ const Cards = () => {
         flexWrap: "wrap",
         minWidth: 300,
         width: "100%",
-        marginTop: "100px",
+        marginTop: "80px",
         justifyContent: "space-around",
       }}
     >
--- src/views/App.js
+++ src/views/App.js
@@ -2,19 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "../commons/navbar/Navbar"
 import Footer from "../commons/footer/Footer"
 import Home from "../components/home/Home"
-import { ThemeProvider } from '@mui/material/styles';
-import theme from "./themeConfig"
  
 function App() {
   return (
     <div>
-    <ThemeProvider theme={theme}>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </ThemeProvider>
     </div>
   );
 }
