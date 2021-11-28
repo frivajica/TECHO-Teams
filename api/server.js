7c913dcacfc710331a603ffabc5d4758f2b18e81
joaquin <joaquin.ecker@gmail.com>
2021-11-28 18:24:09 -0300

    Logueo desde api actividades

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const app = express();
 const morgan = require("morgan");
 const db = require("./config/database");
 const routes = require("./routes");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -11,11 +14,11 @@ app.use(morgan("combined"));
 app.use("/api", routes);
 
 db.sync({ force: false })
-.then(() => {
-  app.listen(3001, () =>
-    console.log(
-      "BACKEND HERE!! db sync done \nServidor escuchando en el puerto 3001 :D"
-    )
-  )
-})
-.catch(err => console.log({ err }))
+  .then(() => {
+    app.listen(3001, () =>
+      console.log(
+        "BACKEND HERE!! db sync done \nServidor escuchando en el puerto 3001 :D"
+      )
+    );
+  })
+  .catch((err) => console.log({ err }));
--- src/state/usuario.js
+++ src/state/usuario.js
@@ -1,15 +1,27 @@
 import {
-    createAction,
-    createAsyncThunk,
-    createReducer,
+  createAction,
+  createAsyncThunk,
+  createReducer,
 } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const setUsuario = createAction("SET_USARIO");
 
-const usuarioReducer = createReducer({},
-    {
-        [setUsuario]: (state, action) => action.payload,
-    }
+export const loginRequest = createAsyncThunk("LOGIN", ({ email, password }) => {
+  return axios
+    .post("https://sandbox.actividades.techo.org/api/login", {
+      email,
+      password,
+    })
+    .then((res) => console.log(res));
+});
+
+const usuarioReducer = createReducer(
+  {},
+  {
+    [setUsuario]: (state, action) => action.payload,
+    [loginRequest.fulfilled]: (state, action) => action.payload,
+  }
 );
 
 export default usuarioReducer;
