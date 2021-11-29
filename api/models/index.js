528d8e33d1c8079952d84355e6813cfc1720afea
juanmcost <jmcostanzo@gmail.com>
2021-11-29 16:19:46 -0300

    historial equipos

--- api/models/index.js
+++ api/models/index.js
@@ -2,16 +2,16 @@ const Equipo = require("./Equipo");
 const Usuario = require("./Usuario");
 const Role = require("./Role");
 const UsuarioEnEquipo = require("./UsuarioEnEquipo");
-// const Actividad = require("./Actividades");
-
-
-//Equipo.hasMany(Actividad);
+const Evento = require("./Evento");
 
 Usuario.belongsToMany(Equipo, {through: UsuarioEnEquipo});
 Equipo.belongsToMany(Usuario, {through: UsuarioEnEquipo});
 
 Role.hasMany(UsuarioEnEquipo, {as: "usrEnEquipo"});
 UsuarioEnEquipo.belongsTo(Role);
+
+Equipo.hasMany(Evento);
+Evento.belongsTo(Equipo);
 
 
 module.exports = { Usuario, Equipo, Role, UsuarioEnEquipo };
