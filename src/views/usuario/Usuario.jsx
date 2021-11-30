2aed374e551dd7e5be6137419ee1fe1bb5da1b3e
Fran <fraivan_1@hotmail.com>
2021-11-30 12:39:11 -0600

    Diseño responsive con grid para la vista pública de usuario

--- src/views/usuario/Usuario.jsx
+++ src/views/usuario/Usuario.jsx
@@ -1,42 +1,18 @@
-import { usuario as u } from "../../utils/mockData";
-import Grid from "@mui/material/Grid";
-import ButtonBase from "@mui/material/ButtonBase";
-import Paper from "@mui/material/Paper";
 import "./Usuario.css";
 import Divider from "@mui/material/Divider";
+import { TarjetaUsuario } from "../../components/tarjetaUsuario/TarjetaUsuario";
 
 export const Usuario = () => {
   return (
     <div className="contenedor">
-      <Paper sx={{ p: 2, margin: "3%", maxWidth: "90%", flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase sx={{ width: 200, height: 200 }}>
-              <img className="avatar" src={u.avatar} alt="Avatar de Usuario" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <h1>{`${u.nombres} ${u.apellidoPaterno}`}</h1>
-                <h3>Contenedor de roles</h3>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <h4>Antigüedaddddddd</h4>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
+      <TarjetaUsuario />
 
       <Divider variant="middle" className="divisor" />
-
-      <p class="numeroProyectos">
-        Número de proyectos en los que ha participado: No.
+      <p class="participaciones">
+        Participando en
+        <span className="num-proyectos">{` ${`### proyectos`} `}</span>
       </p>
-
       <Divider variant="middle" className="divisor" />
-
       {
         //historia (Actividades en las que ha participado, sus fechas y sus roles en ellas) //https://actividades.techo.org/ajax/actividades/20786
       }
