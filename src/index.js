a6827c1380427aaa0d5ab46864a5169b352330ae
Fran <fraivan_1@hotmail.com>
2021-12-03 10:30:04 -0600

    Comenzando habilitación de pwa

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/React">
         <React.StrictMode>
           <App />
         </React.StrictMode>
