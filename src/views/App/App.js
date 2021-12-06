5631ec3d0fd5a2411728ddb8b1d8ac0bbf411ca6
eber-cba <ebercoronel29@gmail.com>
2021-12-06 11:30:32 -0300

    miEquipo

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -5,6 +5,7 @@ import Footer from "../../commons/footer/Footer";
 import Home from "../../components/home/Home";
 import SignUp from "../../components/completarSignUp/SignUp";
 import Register from "../../components/Register/Register";
+import Equipo from "../miEquipo/Equipo";
 import { Usuario } from "../usuario/Usuario";
 import MiInformación from "../miInformación/MiInformación";
 import { useSelector } from "react-redux";
@@ -30,6 +31,7 @@ function App() {
             path="/registro"
             element={!usuario.nombres ? <Register /> : <Home />}
           />
+
           <Route
             exact
             path={`/${usuario.idPersona}`}
@@ -48,6 +50,7 @@ function App() {
               )
             }
           />
+          <Route exact path="/miEquipo" element={<Equipo />} />
         </Routes>
       </div>
       <Footer />
