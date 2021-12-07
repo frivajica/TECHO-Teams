a7a33d4db5dc25e2325b39114ac447c532af04a6
Fran <fraivan_1@hotmail.com>
2021-12-07 14:57:22 -0600

    Avances de conformación de equipos (Se añadió parte de la lógica para mostrar los datos que vengan del back)

--- src/components/tarjetaRoles/TarjetaRoles.jsx
+++ src/components/tarjetaRoles/TarjetaRoles.jsx
@@ -1,57 +1,55 @@
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { useState } from "react";
 import ButtonBase from "@mui/material/ButtonBase";
 import { Autocompletar } from "../../commons/autocompletar/Autocompletar";
 import SaveAsRoundedIcon from "@mui/icons-material/SaveAsRounded";
-import  useForm  from "../../hooks/formState";
+import { opcionesPersona,  } from "../../utils/mockData";
+import useForm from "../../hooks/formState";
+import TextField from "@mui/material/TextField";
 import "./TarjetaRoles.css";
-const imagen = "https://imco.org.mx/wp-content/uploads/2020/08/test-3.png";
-const values = ["hola", "como", "estas"];
-const predeterminado = "hola";
 
 export const TarjetaRoles = () => {
-	const { form, handleChange } = useForm();
-  const [roles, setRoles] = useState("");
-  const [saveBtn, setSaveBtn] = useState(false);
-  const handleSelect = (event) => {		
+  const { form, handleChange } = useForm();
+  const handleSelect = (event) => {
+    console.log(event)
     handleChange(event);
-		console.log(form)
   };
 
   return (
     <div className="tarjeta-roles">
       <div className="rol-imagen">
         <ButtonBase sx={{ width: 200, height: 200 }} id="ripple-avatar">
-          <img className="avatar" src={imagen} alt="Avatar de Usuario" />
+          <img className="avatar" src={'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="Avatar de Usuario" />
         </ButtonBase>
       </div>
       <div className="rol-opciones">
         <FormControl id="modificar-rol" variant="standard">
-          <Autocompletar
-            opciones={values}
-            etiqueta="Rol"
-            onSelect={handleSelect}
-            valorPred={predeterminado}
-            nombre="rol"
+          <TextField
+            onChange={handleSelect}
+            defaultValue={'opcionesRol[0].titulo'}
+            name="rol"
+            label="Rol"
+            placeholder="Rol"
+            variant="standard"
           />
         </FormControl>
         <div id="buscar-persona">
           <Autocompletar
-            opciones={values}
+            valorPred={{titulo: 'hola'}}
+            opciones={opcionesPersona}
             etiqueta="Persona"
-            onSelect={handleSelect}
+            onChange={handleSelect}
             nombre="Persona"
           />
         </div>
         <FormControlLabel
           id="checkbox-rol"
-          control={<Checkbox />}
+          control={<Checkbox defaultChecked={true}  onChange={handleSelect} name="necesario" />}
           label="Necesario"
         />
       </div>
-      {saveBtn && (
+      {form && (
         <ButtonBase id="save-icon">
           <SaveAsRoundedIcon color="action" />
         </ButtonBase>
