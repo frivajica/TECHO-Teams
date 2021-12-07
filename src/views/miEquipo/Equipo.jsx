380cef8302ecb8e8cf89750aa93e29c4b56c4f6d
eber-cba <ebercoronel29@gmail.com>
2021-12-07 17:07:35 -0300

    eber-vistaEquipo

--- src/views/miEquipo/Equipo.jsx
+++ src/views/miEquipo/Equipo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Conformacion from './conformacion/Conformacion'
 function Equipo() {
     return (
         <div>
-            <p>conformación</p>
+           
+            <Conformacion/>
         </div>
     )
 }
