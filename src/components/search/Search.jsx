d85ec742f8158f08001c7f881539842d5cfc691b
joaquin <joaquin.ecker@gmail.com>
2021-12-07 15:09:25 -0300

    Vista search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { CustomHook } from "../../hooks/CustomHook";
+import Button from "@mui/material/Button";
 
 export default function Search() {
   const [tipo, setTipo] = useState("");
   const busqueda = CustomHook("");
+  const handleSubmit = () => {};
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <div
-        className="searchDiv"
-        style={{ display: "flex", alignItems: "center" }}
-      >
+      <form className="searchDiv" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="selector" className="label" required>
             <p>BUSCAR POR </p>
@@ -54,7 +53,12 @@ export default function Search() {
             />
           </label>
         </div>
-      </div>
+        <div>
+          <Button id="ingresar" size="medium" variant="outlined" type="submit">
+            BUSCAR
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
