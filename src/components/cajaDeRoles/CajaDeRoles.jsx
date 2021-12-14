88cb7b2fc5b7c800e8642ef02b81b0370b51b193
eber-cba <ebercoronel29@gmail.com>
2021-12-14 16:05:32 -0300

    skeleton&userAdmin

--- src/components/cajaDeRoles/CajaDeRoles.jsx
+++ src/components/cajaDeRoles/CajaDeRoles.jsx
@@ -5,6 +5,7 @@ import Chip from "@mui/material/Chip";
 import Box from "@mui/material/Box";
 import ButtonBase from "@mui/material/ButtonBase";
 import "./CajaDeRoles.css";
+import Skeleton from "@mui/material/Skeleton";
 
 export const CajaDeRoles = () => {
   const dispatch = useDispatch();
@@ -31,11 +32,15 @@ export const CajaDeRoles = () => {
   return (
     <div className="caja">
       <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }} id="roles">
-        {roles.map((value, i) => (
-          <ButtonBase id="ripple" key={i}>
-            {value && <Chip label={value} color="primary" />}
-          </ButtonBase>
-        ))}
+        {!roles.length ? ([1,2].map((n)=>   <Skeleton key={n} sx={{width:120,height:50, borderRadius:5}} /> )
+       
+        ) : (
+          roles.map((value, i) => (
+            <ButtonBase id="ripple" key={i}>
+              {value && <Chip label={value} color="primary" />}
+            </ButtonBase>
+          ))
+        )}
       </Box>
     </div>
   );
