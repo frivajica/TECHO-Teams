eb9536a78d72a090571ff8b90423cf8ae2150c53
Martin Paladino <c.martin.pal@gmail.com>
2021-12-17 15:30:28 -0300

    last

--- api/config/database.js
+++ api/config/database.js
@@ -1,11 +1,12 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("dbtecho", "root", "password", {
-  //host: "bwgpm6fqaz25rnk0nnmj-mysql.services.clever-cloud.com",
+const db = new Sequelize("bwgpm6fqaz25rnk0nnmj", "uglfayeryrkjedwt", "WVymCqqIlnz4sHPY1WNH", {
+  host: "bwgpm6fqaz25rnk0nnmj-mysql.services.clever-cloud.com",
   dialect: "mysql",
   logging: false,
   timezone: "-03:00",
 });
+
 
 
 module.exports = db;
